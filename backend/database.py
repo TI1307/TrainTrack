f335114034670a0d7f2dfd3a5079893031ab955f
Belkeis <belkeis.salhi@ensia.edu.dz>
2026-08-14 09:24:47 +0100

    edits to add db to postgressql ( Neon )

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine , event
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import os
 
-DATABASE_URL = "sqlite:///./traintrack.db"
 
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./traintrack.db")
+
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 #The engine is the object that actually knows how to open a connection to that file and speak SQL to it
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
                             # don't silently save changes to the DB until you explicitly tell it to (db.commit())
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -19,8 +23,10 @@ def get_db():
         db.close()
 
 # run this whenever  connection is set , and apply foreign Key Rules ( assign non existing foreign key , or delete a record in main table )
-@event.listens_for(engine, "connect")
-def enable_foreign_keys(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
+# database.py
+if DATABASE_URL.startswith("sqlite"):
+    @event.listens_for(engine, "connect")
+    def enable_foreign_keys(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
